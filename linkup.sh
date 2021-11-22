a8a69d805265621b39abb456a566a19f58d112a6
M.B. Christiansen <mbc@capmon.dk>
2021-11-22 19:06:41 +0100

    Add linkup

---
+++ linkup.sh
@@ -0,0 +1,19 @@
+dotfiles="$HOME/documents/dotfiles"
+ln -s $HOME/documents/dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/documents/dotfiles/.bashrc.d $HOME/.bashrc.d
+ln -s $HOME/documents/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/documents/dotfiles/.vim $HOME/.vim
+ln -s $HOME/documents/dotfiles/.bin $HOME/.bin
+ln -s $HOME/documents/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/documents/dotfiles/.Xresources $HOME/.Xresources
+cp $HOME/documents/dotfiles/.gitconfig_secret $HOME/.gitconfig_secret
+vim .gitconfig_secret
+
+git clone https://github.com/todotxt/todo.txt-cli.git tools/todo.txt
+ln -s $HOME/documents/dotfiles/tools/todo.txt/todo.sh $HOME/documents/dotfiles/.bin/todo.sh
+
+
+# wget pictures to $HOME/pictures
+# wget notes $HOME/notes
+# sync music to /srv/music
+# sync todo to $HOME/todo
