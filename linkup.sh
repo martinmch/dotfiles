277502fa6030808865c499be852ba42739a59a89
M.B. Christiansen <mbc@capmon.dk>
2021-11-25 15:40:59 +0100

    Update linkup

--- linkup.sh
+++ linkup.sh
@@ -1,20 +1,34 @@
 dotfiles="$HOME/documents/dotfiles"
-ln -s $HOME/documents/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/documents/dotfiles/.bashrc.d $HOME/.bashrc.d
-ln -s $HOME/documents/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/documents/dotfiles/.vim $HOME/.vim
-ln -s $HOME/documents/dotfiles/.bin $HOME/.bin
-ln -s $HOME/documents/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/documents/dotfiles/.Xresources $HOME/.Xresources
+ln -s $HOME/documents/dotfiles/.bashrc           $HOME/.bashrc
+ln -s $HOME/documents/dotfiles/.bashrc.d         $HOME/.bashrc.d
+ln -s $HOME/documents/dotfiles/.vimrc            $HOME/.vimrc
+ln -s $HOME/documents/dotfiles/.vim              $HOME/.vim
+ln -s $HOME/documents/dotfiles/.bin              $HOME/.bin
+ln -s $HOME/documents/dotfiles/.gitconfig        $HOME/.gitconfig
+ln -s $HOME/documents/dotfiles/.Xresources       $HOME/.Xresources
 ln -s $HOME/documents/dotfiles/.config/i3/config $HOME/.config/i3/config
-cp $HOME/documents/dotfiles/.gitconfig_secret $HOME/.gitconfig_secret
-vim .gitconfig_secret
 
-git clone https://github.com/todotxt/todo.txt-cli.git tools/todo.txt
-ln -s $HOME/documents/dotfiles/tools/todo.txt/todo.sh $HOME/documents/dotfiles/.bin/todo.sh
+setup_gitsecret () {
+    echo<<EOF > .gitconfig_secret
+[user]
+    name  =
+    email =
+EOF
+    vim .gitconfig_secret
+    cp    $HOME/documents/dotfiles/.gitconfig_secret $HOME/.gitconfig_secret
+}
+
+install_firefox () {
+    # Download firefox to /opt/firefox/
+    # chmod -R 755 /opt/firefox
+    # ln -s /opt/firefox/firefox /usr/bin/firefox
+}
 
 
-# wget pictures to $HOME/pictures
-# wget notes $HOME/notes
-# sync music to /srv/music
-# sync todo to $HOME/todo
+# git clone notes $HOME/notes
+# git clone todo $HOME/todo
+
+# Nextcloud:
+#   music
+#   pictures
+#   books
