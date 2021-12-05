bda2292d2f0496fa8bedade491a7d1a6020bfe63
Christiansen <me@martinmch.com>
2021-12-05 23:14:30 +0100

    Extend linkup

--- linkup.sh
+++ linkup.sh
@@ -1,27 +1,72 @@
+#!/usr/bin/env sh
+set -e
+
+if [ ! -d ~/.ssh ]; then
+    echo "Please setup ssh key to continue link up"
+    exit 1
+fi
+
+rm -f   ~/.bashrc \
+        ~/.vimrc \
+        ~/.Xresources
+
+for f in .bash_history .sqlite_history .mysql_history; do
+    rm -f ~/$f
+    ln -s /dev/null ~/$f
+done
+chmod 700 ~
+
 dotfiles="$HOME/documents/dotfiles"
-ln -s $HOME/documents/dotfiles/.bashrc           $HOME/.bashrc
-ln -s $HOME/documents/dotfiles/.bashrc.d         $HOME/.bashrc.d
-ln -s $HOME/documents/dotfiles/.vimrc            $HOME/.vimrc
-ln -s $HOME/documents/dotfiles/.vim              $HOME/.vim
-ln -s $HOME/documents/dotfiles/.bin              $HOME/.bin
-ln -s $HOME/documents/dotfiles/.gitconfig        $HOME/.gitconfig
-ln -s $HOME/documents/dotfiles/.Xresources       $HOME/.Xresources
-ln -s $HOME/documents/dotfiles/.config/i3/config $HOME/.config/i3/config
+
+if [ -d "$dotfiles" ]; then
+    cd "$dotfiles"
+    git pull --ff-only
+else
+    if [ -d ~/.ssh ]; then
+        remote="git@github.com:martinmch/dotfiles.git"
+    else
+        remote="https://github.com/martinmch/dotfiles.git"
+    fi
+    mkdir -p $(dirname $dotfiles)
+    git clone "$remote" "$dotfiles"
+fi
+
+(
+    cd "$dotfiles"
+    for f in .???*; do
+        rm -f ~/$f
+        (cd ~/; ln -s "$dotfiles"/$f $f)
+    done
+)
+
+
+# Done linking up dotfiles.
+# Now we need to set up secrets, files and applications.
 
 setup_gitsecret () {
-    echo<<EOF > .gitconfig_secret
+    echo<<EOF > ~/.gitconfig_secret
 [user]
     name  =
     email =
 EOF
-    vim .gitconfig_secret
-    cp    $HOME/documents/dotfiles/.gitconfig_secret $HOME/.gitconfig_secret
+    vim ~/.gitconfig_secret
 }
 
 install_firefox () {
-    # Download firefox to /opt/firefox/
-    # chmod -R 755 /opt/firefox
-    # ln -s /opt/firefox/firefox /usr/bin/firefox
+    if [[ "$(whoami)" != 'root' ]];
+    then
+        # This is a lie. We're not checking if we can access opt.
+        dieError "cannot access '/opt/firefox/': Permission denied"
+    fi
+
+    firefoxURL="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+    wget -qO "$firefoxURL" firefox.tar.bz2
+    mkdir -p /opt/firefox/
+    tar -xf firefox.tar.bz2 /opt/firefox/
+    chmod -R 755 /opt/firefox
+    mv /usr/bin/firefox /usr/bin/firefox.apt
+    ln -s /opt/firefox/firefox /usr/bin/firefox
+    rm firefox.tar.bz2
 }
 
 
