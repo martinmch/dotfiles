d61abe2e3e14741ba4ccf665969f19897fc17d95
M.B. Christiansen <mbc@capmon.dk>
2022-01-10 17:39:24 +0100

    Update screen setup

--- .bin/work-screen-layout.sh
+++ .bin/work-screen-layout.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-xrandr \
-    --output DP-2-1 --mode 2560x1440 --right-of DP-2-2 \
-    --output DP-2-2 --primary --mode 2560x1440 \
-    --output eDP-1 --off \
-    --output HDMI-1 --off \
-    --output DP-2 --off \
-    --output DP-1 --off 
 
-# xrandr \
-#     --output DP-2-1 --off \
-#     --output DP-2-2 --off \
-#     --output eDP-1 --primary --mode 1920x1080 \
-#     --output HDMI-1 --off \
-#     --output DP-2 --off \
-#     --output DP-1 --off 
+#if [ $(cat /sys/devices/platform/dock.0/docked) ]; then
+#    xrandr \
+#        --output DP-2-1 --mode 2560x1440 --right-of DP-2-2 \
+#        --output DP-2-2 --primary --mode 2560x1440 \
+#        --output eDP-1 --off \
+#        --output HDMI-1 --off \
+#        --output DP-2 --off \
+#        --output DP-1 --off
+#fi
 
 
-feh --bg-scale ~/pictures/big-sur.jpeg
+bash $HOME/.fehbg
 
-setxkbmap us # -variant altgr-intl
+setxkbmap us
