2b2558b6f0ad53d1a60c5d398e8cf3c9f04fb2f0
M.B. Christiansen <mbc@capmon.dk>
2022-09-27 10:49:32 +0200

    Use mpd as client for music

---
+++ .bashrc.d/mpc-queue.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+alias mpc='mpc -f "\[%position%\] %artist% - %album% - %title%"'
+
+alias mpc-queue='mpc playlist -f "%position% %artist% - %title%" \
+        | grep --color=always -B 5 -A 5 -E "$(mpc current)"'
+
