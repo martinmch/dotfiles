5c682779919358ab43eee6b28a5e8659b2ba81a6
Martin Christiansen <mbc@capmon.dk>
2022-11-21 14:28:54 +0100

    Fix formatting in mpc alias

--- .bashrc.d/mpc-queue.bash
+++ .bashrc.d/mpc-queue.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-alias mpc='mpc -f "\[%position%\] %artist% - %album% - %title%"'
+alias mpc='mpc -f "\[%position%\] [[[%artist% - ][%album% - ]%title%]|[%file%]]"'
 
 alias mpc-queue='mpc playlist -f "%position% %artist% - %title%" \
         | grep --color=always -B 5 -A 5 -E "$(mpc current)"'
-
